ca16afb0117e6fbbc9537964d699d030ee253070
Nhan Tam <nhantamhd@gmail.com>
2024-06-18 17:31:10 +0700

    Tam commit

--- src/app/homePage/projectMember/page.tsx
+++ src/app/homePage/projectMember/page.tsx
@@ -1,6 +1,5 @@
 import FilterProjectMember from "@/components/projectMember/Filter";
 import TableProjectMember from "@/components/projectMember/Table";
-import ButtonGroupMemberProject from "@/components/projectMember/ButtonGroup";
 const projectMember = () => {
     return (
         <div>
